44285f9cf80d96c3f8042d8c20d19a2cc917f0ce
Ignacio Vizzo <ignaciovizzo@gmail.com>
2021-11-18 12:23:18 +0100

    Do some C++ magic to get type names at compile time

--- ScopeTimer.h
+++ ScopeTimer.h
@@ -11,6 +11,39 @@
 #include <ratio>
 #include <string>
 
+namespace type_traits {
+
+template <typename>
+struct get_string_type {
+    static char const *value();
+};
+
+#define REGISTER_PERIOD_TYPE(T)                             \
+    template <>                                             \
+    struct get_string_type<T> {                             \
+        static char const *value() { return #T "seconds"; } \
+    };
+
+using namespace std;
+REGISTER_PERIOD_TYPE(atto);
+REGISTER_PERIOD_TYPE(femto);
+REGISTER_PERIOD_TYPE(pico);
+REGISTER_PERIOD_TYPE(nano);
+REGISTER_PERIOD_TYPE(micro);
+REGISTER_PERIOD_TYPE(milli);
+REGISTER_PERIOD_TYPE(centi);
+REGISTER_PERIOD_TYPE(deci);
+REGISTER_PERIOD_TYPE(deca);
+REGISTER_PERIOD_TYPE(hecto);
+REGISTER_PERIOD_TYPE(kilo);
+REGISTER_PERIOD_TYPE(mega);
+REGISTER_PERIOD_TYPE(giga);
+REGISTER_PERIOD_TYPE(tera);
+REGISTER_PERIOD_TYPE(peta);
+REGISTER_PERIOD_TYPE(exa);
+
+}  // namespace type_traits
+
 // Pick on of this clock ratios, defined in the <ratio> header file
 // typedef ratio<1,       1000000000000000000> atto;
 // typedef ratio<1,          1000000000000000> femto;
@@ -46,7 +79,10 @@ public:
 private:
     inline void Start() { start_time_ = GetSystemTime(); }
     inline void Stop() { end_time_ = GetSystemTime(); }
-    inline void Print() { fmt::print("{} took {:.2f} ms.\n", timer_info_, GetDuration()); }
+    inline void Print() {
+        fmt::print("{} took {:.2f} {}.\n", timer_info_, GetDuration(),
+                   type_traits::get_string_type<Period>::value());
+    }
     inline FloatT GetDuration() { return end_time_ - start_time_; }
     inline FloatT GetSystemTime() {
         TimeT current_time = std::chrono::high_resolution_clock::now().time_since_epoch();
