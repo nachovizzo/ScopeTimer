4f64759039b79198cec8dc1df3058cea892b402f
Ignacio Vizzo <ignaciovizzo@gmail.com>
2021-11-18 12:40:57 +0100

    Simplify implementation

--- ScopeTimer.h
+++ ScopeTimer.h
@@ -44,53 +44,28 @@ REGISTER_PERIOD_TYPE(exa);
 
 }  // namespace type_traits
 
-// Pick on of this clock ratios, defined in the <ratio> header file
-// typedef ratio<1,       1000000000000000000> atto;
-// typedef ratio<1,          1000000000000000> femto;
-// typedef ratio<1,             1000000000000> pico;
-// typedef ratio<1,                1000000000> nano;
-// typedef ratio<1,                   1000000> micro;
-// typedef ratio<1,                      1000> milli;
-// typedef ratio<1,                       100> centi;
-// typedef ratio<1,                        10> deci;
-// typedef ratio<                       10, 1> deca;
-// typedef ratio<                      100, 1> hecto;
-// typedef ratio<                     1000, 1> kilo;
-// typedef ratio<                  1000000, 1> mega;
-// typedef ratio<               1000000000, 1> giga;
-// typedef ratio<            1000000000000, 1> tera;
-// typedef ratio<         1000000000000000, 1> peta;
-// typedef ratio<      1000000000000000000, 1> exa;
 template <typename Period = std::milli, typename FloatT = double>
 class ScopeTimer {
 public:
     using TimeT = std::chrono::duration<FloatT, Period>;
-    ScopeTimer(const std::string &timer_info) : timer_info_(timer_info) { Start(); }
-    ~ScopeTimer() {
-        Stop();
-        Print();
+    ScopeTimer(const std::string &timer_info) : timer_info_(timer_info) { start_time_ = tic(); }
+    ~ScopeTimer() { fmt::print("{} took {:.2f} {}.\n", timer_info_, GetDuration(), GetPeriodType()); }
+
+private:
+    inline FloatT GetDuration() const { return tic() - start_time_; }
+    inline std::string GetPeriodType() const { return type_traits::get_string_type<Period>::value(); }
+    inline FloatT tic() const {
+        TimeT current_time = std::chrono::high_resolution_clock::now().time_since_epoch();
+        return current_time.count();
     }
 
+public:
     // Avoid strange creation of the ScopteTimer object
     ScopeTimer() = delete;
     ScopeTimer(const ScopeTimer &) = delete;
     void operator=(const ScopeTimer &) = delete;
 
 private:
-    inline void Start() { start_time_ = GetSystemTime(); }
-    inline void Stop() { end_time_ = GetSystemTime(); }
-    inline void Print() {
-        fmt::print("{} took {:.2f} {}.\n", timer_info_, GetDuration(),
-                   type_traits::get_string_type<Period>::value());
-    }
-    inline FloatT GetDuration() { return end_time_ - start_time_; }
-    inline FloatT GetSystemTime() {
-        TimeT current_time = std::chrono::high_resolution_clock::now().time_since_epoch();
-        return current_time.count();
-    }
-
-private:
     FloatT start_time_ = 0.0;
-    FloatT end_time_ = 0.0;
     std::string timer_info_;
 };
